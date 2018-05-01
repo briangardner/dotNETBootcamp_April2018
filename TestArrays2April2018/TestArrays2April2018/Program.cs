46c13a5756e2873f484f933f04ba2b2a35228fca
kamel-rush <kamel@grandcircus.co>
2018-05-01 13:42:19 -0400

    demos for arrays

---
+++ TestArrays2April2018/TestArrays2April2018/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArrays2April2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Design a program that reads the number of students
+            // then creates an array of type double
+            // The program will read the grades for students
+            // then find the max and min grade 
+
+            // 1. Read the number of students and save it in a variable
+            Console.WriteLine("Please enter the amount of students");
+            int amtStudents = int.Parse(Console.ReadLine());
+
+            // 2. Create an array of type double with the size amtStudents
+            double[] Students = new double[amtStudents];
+
+            // 3. Ask the user to enter the grades (Input step)
+            for (int i = 0; i < Students.Length; i++)
+            {
+                Console.WriteLine($"Please enter a grade for student No.{i + 1}");
+                double input = double.Parse(Console.ReadLine());
+                Students[i] = input;
+;            }
+        }
+    }
+}
---
+++ TestArraysApril2018/TestArraysApril2018/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArraysApril2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] EmployeeAges = new int[10];
+
+
+            int[] EmployeeAges; // Declare step 
+            EmployeeAges = new int[4]; // initialize step
+
+            //for (int i = 0; i < EmployeeAges.Length; i++)
+            //{
+            //    Console.WriteLine(EmployeeAges[i]);
+            //}
+
+            // Get the user to enter the values for the elements 
+            for (int i = 0; i < EmployeeAges.Length; i++)
+            {
+                Console.WriteLine($"Please enter an Age of Employee No.{i+1}");
+                int Input = int.Parse(Console.ReadLine());
+                EmployeeAges[i] = Input; 
+            }
+
+
+            // processing 
+            // 1. Avg 
+            // 2. Min, Max
+
+
+            // Avg: Sum then divide on Length 
+
+            float Sum = 0;
+
+            for (int i = 0; i < EmployeeAges.Length; i++)
+            {
+                Sum = Sum + EmployeeAges[i];
+            }
+
+            // Find Avg: 
+
+            // output step 
+            float Avg = Sum / EmployeeAges.Length;
+
+            Console.WriteLine(Avg);
+
+
+
+           
+
+        }
+    }
+}
