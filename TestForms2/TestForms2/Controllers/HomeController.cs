af77052ecb7e67bd3b9b41a5254ac896087512f6
kamel-rush <kamel@grandcircus.co>
2018-05-23 15:05:09 -0400

    Models

---
+++ TestForms2/TestForms2/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestForms2.Models;
+
+namespace TestForms2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SignUpForm()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewUser(UserInfo newUser )
+        {
+            // Validation! 
+            if (ModelState.IsValid)
+            {
+                // to insert the newUser into the DB! 
+
+                ViewBag.Message = $"Welcome {newUser.Username}";
+                return View("Confirm");
+            }
+            else
+            {
+                ViewBag.Address = Request.UserAgent;
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ TestForms2/TestForms2/Models/UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestForms2.Models
+{
+    public class UserInfo
+    {   
+        [Required]
+        public string Username { set; get; }
+
+        [Required]
+        public string Password { set; get; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-] +)@([\w\-] +)((\.(\w){2, 3})+)$",ErrorMessage ="Bad Email!")]
+        public string Email { set; get; }
+
+        public UserInfo()
+        {
+        }
+        public UserInfo(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+    }
+}
